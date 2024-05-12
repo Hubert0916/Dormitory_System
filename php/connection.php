337530172e5b8ab9a96dc8b9557353fbfc9e1c32
yuhan <h237k77777@gmail.com>
2024-05-12 23:56:51 +0800

    feat: Update database connection details
    Update the database connection details in the `connection.php` file to connect to the new server with the IP address `104.199.139.129`. This change ensures that the application can establish a successful connection to the database.

---
+++ php/connection.php
@@ -0,0 +1,15 @@
+<?php
+	$servername = "104.199.139.129";
+	$username = "rdu";
+	$password = "1234";
+	$db_name = "mysql";
+	$conn = new mysqli($servername, $username, $password, $db_name);
+
+	if($conn->connect_error)
+	{
+		die("Connection failed. $conn->connect_error");
+	}
+	else
+		echo "Hello, " . $username . "!";
+
+?>
