06d31c01abd14529d80ae0b90c27f61ab285650c
鍾芸琦 <chung38.mg11@nycu.edu.tw>
2024-05-13 11:04:20 +0800

    Create web.php

---
+++ php/web.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+</head>
+<body>
+    <h1>Welcome to the Homepage</h1>
+    
+    <?php
+    // PHP code here
+    ?>
+    
+</body>
+</html>
