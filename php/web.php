8a9b24f4213d8c0a7fea37c9cb1577548b2b9e48
鍾芸琦 <chung38.mg11@nycu.edu.tw>
2024-05-14 23:36:26 +0800

    feat: design

--- php/web.php
+++ php/web.php
@@ -5,17 +5,34 @@
     <link rel="stylesheet" type="text/css" href="home.css">
 </head>
 <body>
+    <nav id="main-nav" class="navbar navbar-expand-sm navbar-dark fixed-top" >
+    <nav class="nav navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#Navbarshow">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span> 
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+        <div class="collapse navbar-collapse" id="Navbarshow"> 
+            <ul class="nav navbar-nav">
+                <li><a data-toggle="tab" href="#home">HOME</a></li>
+                <li><a data-toggle="tab" href="#menu1">SERVICES</a></li>
+                <li><a data-toggle="tab" href="#menu2">PROFILE</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>
+                <li><a href="#">Log out</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Admin</a></li>
+            </ul>
+        </div>
+    </div>
+    </nav> 
     <div class="container">
         <div class="header">
             <h1>Dormitory System</h1>
-        </div>
-        <div class="menu">
-            <ul>
-                <li><a href="home.php">Home</a></li>
-                <li><a href="room.php">Room</a></li>
-                <li><a href="student.php">Student</a></li>
-                <li><a href="report.php">Report</a></li>
-            </ul>
         </div>
         <div class="content">
             <h2>Home</h2>
