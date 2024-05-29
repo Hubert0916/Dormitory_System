f265fe90998f6d84159b617b62337da5ce0969b8
Ooscot421 <ahankeu90@gmail.com>
2024-05-30 03:11:33 +0800

    Update move_match.php

--- php/move_match.php
+++ php/move_match.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'connection.php';
-require_once dirname(__FILE__) . "/overlay_nav.php";
+//require_once dirname(__FILE__) . "/overlay_nav.php";
 
 // Check connection
 if ($conn->connect_error) {
