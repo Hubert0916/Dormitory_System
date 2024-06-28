e11bc98a094e2e045eb4fc6906c42b9e5f857ed0
Hubert <h237k77777@gmail.com>
2024-06-28 21:15:18 +0800

    update connection.php to connect to local mysql

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,7 @@
 <?php
-	$servername = "104.199.139.129";
+	$servername = "localhost";
 	$username = "root";
-	$password = "1234";
+	$password = "0000";
 	$db_name = "Dorm";
 	$conn = new mysqli($servername, $username, $password, $db_name);
 	
